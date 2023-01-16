6e9bdd6e153ec9a4e719fd1cf5f242c1a563fff7
Vanessa1507 <lauritasuarezg@gmail.com>
2023-01-16 15:20:05 -0500

    Vite config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
+import React from "react"
+import { createRoot } from "react-dom"
+
 const Pet = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.name),
     React.createElement("h2", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-  ]);
-};
+  ])
+}
 
 const App = () => {
   return React.createElement("div", {}, [
@@ -13,9 +16,9 @@ const App = () => {
     React.createElement(Pet, { name: "Arya", animal: "cat", breed: "Mixed" }),
     React.createElement(Pet, { name: "Simón", animal: "cat", breed: "Siames" }),
     React.createElement(Pet, { name: "Ramón", animal: "dog", breed: "Mixed" }),
-  ]);
-};
+  ])
+}
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
-root.render(React.createElement(App));
+const container = document.getElementById("root")
+const root = createRoot(container)
+root.render(React.createElement(App))
