39ab7cde49bda43bcfe4d2d53e72a484a04b462e
Vanessa1507 <lauritasuarezg@gmail.com>
2023-01-16 09:24:15 -0500

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("h2", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Magola", animal: "cat", breed: "Mixed" }),
+    React.createElement(Pet, { name: "Arya", animal: "cat", breed: "Mixed" }),
+    React.createElement(Pet, { name: "Simón", animal: "cat", breed: "Siames" }),
+    React.createElement(Pet, { name: "Ramón", animal: "dog", breed: "Mixed" }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
