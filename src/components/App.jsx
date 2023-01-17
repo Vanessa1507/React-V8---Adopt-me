5c008b47d97ab15047082a89849c448d7603f039
Vanessa1507 <lauritasuarezg@gmail.com>
2023-01-16 22:02:26 -0500

    Feat: Add SearchParams component and install a plugin for hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,12 @@
-import { createRoot } from "react-dom"
-import Pet from "./Pet"
+import { createRoot } from "react-dom/client"
+import SearchParams from "./SearchParams"
 //Components
-//Data
-const infoPets = [
-  { name: "Magola", animal: "Cat", breed: "Mixed" },
-  { name: "Arya", animal: "Cat", breed: "Mixed" },
-  { name: "Simón", animal: "Cat", breed: "Siames" },
-  { name: "Ramón", animal: "Dog", breed: "Mixed" },
-]
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      {infoPets.map((pet) => (
-        <Pet name={pet.name} animal={pet.animal} breed={pet.breed} />
-      ))}
+      <SearchParams />
     </div>
   )
 }
