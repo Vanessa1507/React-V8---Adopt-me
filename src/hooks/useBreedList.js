fe00a4dd36e34217ff58ecc6395ff4346d44393d
Vanessa1507 <lauritasuarezg@gmail.com>
2023-01-17 18:06:59 -0500

    Tweak: Refactor useBreedList hook. Feat: Create fetchBreeds async function

--- src/hooks/useBreedList.js
+++ src/hooks/useBreedList.js
@@ -1,34 +1,12 @@
-import { useEffect, useState } from "react";
+import { useQuery } from '@tanstack/react-query';
+//Functions
+import fetchBreeds from '../functions/fetchBreeds';
 
-const localCache = {};
 
 const useBreedList = ({ animal }) => {
-  //states
-  const [breedList, setBreedList] = useState([]);
-  const [status, setStatus] = useState("unloaded");
+  const response = useQuery(['breeds', animal], fetchBreeds);
 
-  useEffect(() => {
-    if (!animal) {
-      setBreedList([]);
-    } else if (localCache[animal]) {
-      setBreedList(localCache[animal]);
-    } else {
-      requestBreedList();
-    }
-    async function requestBreedList() {
-      setBreedList([]);
-      setStatus("loading");
 
-      const res = await fetch(
-        `http://pets-v2.dev-apis.com/breeds?animal=${animal}`
-      );
-      const json = await res.json();
-      localCache[animal] = json.breeds || [];
-      setBreedList(localCache[animal]);
-      setStatus("loaded");
-    };
-  }, [animal]);
-
-  return { breedList, status };
+  return { breedList: response?.data?.breeds ?? [], status: response.status };
 };
 export default useBreedList;
