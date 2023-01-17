45a76ef4fd2a578a680af594f9ea7e83ff756eca
Vanessa1507 <lauritasuarezg@gmail.com>
2023-01-17 17:07:11 -0500

    Feat: Install React Query libray, instantiate the query provider and create the fetchPets async function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,31 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 //Components
 import Details from "./Details";
 import SearchParams from "./SearchParams";
-//Components
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 
 const App = () => {
   return (
     <BrowserRouter>
-      <header>
-        <Link to="/">Adopt Me!</Link>
-      </header>
-      <Routes>
-        <Route path="/" element={<SearchParams />} />
-        <Route path="/details/:id" element={<Details />} />
-      </Routes>
+      <QueryClient client={queryClient}>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
+        <Routes>
+          <Route path="/" element={<SearchParams />} />
+          <Route path="/details/:id" element={<Details />} />
+        </Routes>
+      </QueryClient>
     </BrowserRouter>
   );
 };
