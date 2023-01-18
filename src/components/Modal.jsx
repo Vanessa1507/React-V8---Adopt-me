c4f6eb3307c66a23c82171bbd2643edb0087eee3
Vanessa1507 <lauritasuarezg@gmail.com>
2023-01-18 16:18:03 -0500

    Feat: Create Modal component and add a new node in the index.html file for modals

---
+++ src/components/Modal.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }) => {
+  //We useRef because we need the same thing back every time
+  const elRef = useRef(null);
+
+  //We will create this div once
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    //The node that is on the index.html
+    const modalRoot = document.getElementById("modal");
+    modalRoot.appendChild(elRef.current);
+
+    return () => {
+      modalRoot.removeChild(elRef.current);
+    };
+  }, []);
+
+  return createPortal(<div>{children}</div>);
+};
+
+export default Modal;
